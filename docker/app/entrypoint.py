d47a364b9babe813d8de78278001d3092096cddc
Olivier Martineau <olivier.martineau@irisa.fr>
2026-03-17 07:31:16 +0100

    ci(docker/app/entrypoint.py): lower logs

--- docker/app/entrypoint.py
+++ docker/app/entrypoint.py
@@ -124,5 +124,5 @@ if __name__ == "__main__":
         app,
         host="0.0.0.0",
         port=int(os.environ.get("KRONICLE_PORT", 8000)),
-        log_level="info",
+        log_level="error",
     )
